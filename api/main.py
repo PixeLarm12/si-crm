dfe1edce97a4ce70384f33dbc564e44ad44614ee
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-10-17 13:48:02 -0300

    Added ClientBlueprint

--- api/main.py
+++ api/main.py
@@ -1,19 +1,20 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask,jsonify,request 
-from src.controller import ClientController
+from src.controller.ClientController import client_blueprint
 
+# Necessary to find .env from root/
 dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
 
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 
-apiPrefix = os.getenv('API_PREFIX')
+urlPrefix = os.getenv('API_PREFIX')
 
-@app.route(f"/{apiPrefix}/clients", methods=['GET'])
-def clientsIndex():
-    return jsonify(ClientController.index());
+# Registering ClientController
+app.register_blueprint(client_blueprint, url_prefix=urlPrefix)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
--- api/src/controller/ClientController.py
+++ api/src/controller/ClientController.py
@@ -1,10 +1,15 @@
+from flask import Blueprint
 from src.enum.HttpResponseEnum import HttpResponseEnum
 import src.service.ClientService as clientService
 
-# @index listing clients
-def index():
+client_blueprint = Blueprint('client_controller', __name__)
+
+@client_blueprint.route('/clients')
+def index_clients():
     return {
-        'status': HttpResponseEnum.HTTP_OK,
+        'status': HttpResponseEnum.HTTP_NOT_FOUND,
         'error': False,
         'data': clientService.list()
     };
+
+    
