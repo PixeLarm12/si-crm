f863d91f3897ce23bb24ed7c020afa0e663eb248
PixeLarm <lucasrdomingues-lrd@hotmail.com>
2024-10-18 20:57:47 -0300

    Created userController and userservice

--- api/src/controller/UserController.py
+++ api/src/controller/UserController.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from src.enum.HttpResponseEnum import HttpResponseEnum
 from src.enum.UserEnum import UserEnum
 import src.service.UserService as userService
@@ -7,10 +7,77 @@ user_blueprint = Blueprint('user_controller', __name__)
 
 @user_blueprint.route(f"{UserEnum.BASE_ROUTE}", methods = ['GET'])
 def index_user():
-    return {
-        'status': HttpResponseEnum.HTTP_OK,
-        'error': False,
-        'data': userService.list()
-    };
+    data = userService.list()
 
+    if not data:
+        response = {
+            'status': HttpResponseEnum.HTTP_NO_CONTENT,
+            'data': data
+        }
+    else:
+        response = {
+            'status': HttpResponseEnum.HTTP_OK,
+            'data': data
+        }
+
+    return jsonify(response), response['status']
     
+
+@user_blueprint.route(f"{UserEnum.BASE_ROUTE}", methods = ['POST'])
+def store_user():
+    if request.method != 'POST':
+        response = {
+            'status': HttpResponseEnum.HTTP_FORBIDDEN,
+            'message': {
+                'message': 'This route accepts only POST routes'
+            }
+        }
+
+        return jsonify(response), response['status']
+    
+    if not request.is_json:
+        response = {
+            'status': HttpResponseEnum.HTTP_FORBIDDEN,
+            'message': {
+                'message': 'This route needs header Content-Type: application/json'
+            }
+        }
+
+        return jsonify(response), response['status']
+    
+    data = userService.getRequestData(request.get_json())
+
+    if data['error']:
+        response = { 
+            'status': HttpResponseEnum.HTTP_BAD_REQUEST,
+            'message': data['message']
+        }
+
+        return jsonify(response), response['status']
+     
+    storeResponse = userService.store(request.get_json())
+
+    response = { 
+        'status': HttpResponseEnum.HTTP_CREATED,
+        'data': storeResponse
+    }
+
+    return jsonify(response), response['status']
+
+@user_blueprint.route(f"{UserEnum.BASE_ROUTE}/<id>", methods = ['PUT', 'PATCH'])
+def update_user(id):
+    response = {
+        'status': HttpResponseEnum.HTTP_CREATED,
+        'data': userService.update(id)
+    }
+
+    return jsonify(response), response['status']
+
+@user_blueprint.route(f"{UserEnum.BASE_ROUTE}/<id>", methods = ['DELETE'])
+def delete_user():
+    response = {
+        'status': HttpResponseEnum.HTTP_OK,
+        'data': userService.delete(id)
+    }
+
+    return jsonify(response), response['status']
--- api/src/service/UserService.py
+++ api/src/service/UserService.py
@@ -1,2 +1,47 @@
 def list():
-    return [];
+    return []
+
+def store(data):
+
+    return True
+
+def update(id):
+    return id
+
+def delete(id):
+    return id
+
+def getRequestData(data):
+    messages = ''
+
+    name = data.get('name')
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    confirmPassword = data.get('confirm_password')
+
+    if not name or len(name) < 3:
+        messages += 'The "Name" field are required and need at least 3 characters\n'
+
+    if not username or len(username) < 3:
+        messages += 'The "Username" field are required and need at least 3 characters\n'
+
+    if not email or len(email) < 3:
+        messages += 'The "Email" field are required and need at least 3 characters\n'
+
+    if not password or len(password) < 6:
+        messages += 'The "Password" field are required and need at least 6 characters\n'
+
+    if password != confirmPassword:
+        messages += 'The "Password" and "Confirm Password" fields must match each other\n'
+
+    if len(messages) > 0:
+        return {
+            'error': True,
+            'message': messages
+        }
+    
+    return {
+        'error': False,
+        'message': None
+    }
