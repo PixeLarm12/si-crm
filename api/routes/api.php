1eed08359dd1b3e54623e09177ea4256fed3ca2e
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-11-04 17:13:32 -0300

    Fixed api running docker

--- api/routes/api.php
+++ api/routes/api.php
@@ -10,15 +10,16 @@ use App\Http\Controllers\RatingController;
 use App\Http\Controllers\SaleController;
 use App\Http\Controllers\UserController;
 
+
 /**
  * USERS
  */
 Route::prefix('users')->group(function() {
-    Route::get('/', UserController::class . "@index")->name('users.index');
-    Route::get('/{id}', UserController::class . "@show")->name('users.show');
-    Route::post('/', UserController::class . "@store")->name('users.store');
-    Route::put('/{id}', UserController::class . "@update")->name('users.update');
-    Route::delete('/{id}', UserController::class . "@delete")->name('users.delete');
+    Route::get('/', [UserController::class, "index"])->name('users.index');
+    Route::get('/{id}', [UserController::class, "show"])->name('users.show');
+    Route::post('/', [UserController::class, "store"])->name('users.store');
+    Route::put('/{id}', [UserController::class, "update"])->name('users.update');
+    Route::delete('/{id}', [UserController::class, "delete"])->name('users.delete');
 });
 
 
@@ -26,11 +27,11 @@ Route::prefix('users')->group(function() {
  * ASSISTANCES
  */
 Route::prefix('assistances')->group(function() {
-    Route::get('/', AssistanceController::class . "@index")->name('assistances.index');
-    Route::get('/{id}', AssistanceController::class . "@show")->name('assistances.show');
-    Route::post('/', AssistanceController::class . "@store")->name('assistances.store');
-    Route::put('/{id}', AssistanceController::class . "@update")->name('assistances.update');
-    Route::delete('/{id}', AssistanceController::class . "@delete")->name('assistances.delete');
+    Route::get('/', [AssistanceController::class, "index"])->name('assistances.index');
+    Route::get('/{id}', [AssistanceController::class, "show"])->name('assistances.show');
+    Route::post('/', [AssistanceController::class, "store"])->name('assistances.store');
+    Route::put('/{id}', [AssistanceController::class, "update"])->name('assistances.update');
+    Route::delete('/{id}', [AssistanceController::class, "delete"])->name('assistances.delete');
 });
 
 
@@ -38,11 +39,11 @@ Route::prefix('assistances')->group(function() {
  * GENRES
  */
 Route::prefix('genres')->group(function() {
-    Route::get('/', GenreController::class . "@index")->name('genres.index');
-    Route::get('/{id}', GenreController::class . "@show")->name('genres.show');
-    Route::post('/', GenreController::class . "@store")->name('genres.store');
-    Route::put('/{id}', GenreController::class . "@update")->name('genres.update');
-    Route::delete('/{id}', GenreController::class . "@delete")->name('genres.delete');
+    Route::get('/', [GenreController::class, "index"])->name('genres.index');
+    Route::get('/{id}', [GenreController::class, "show"])->name('genres.show');
+    Route::post('/', [GenreController::class, "store"])->name('genres.store');
+    Route::put('/{id}', [GenreController::class , "update"])->name('genres.update');
+    Route::delete('/{id}', [GenreController::class . "delete"])->name('genres.delete');
 });
 
 
@@ -50,11 +51,11 @@ Route::prefix('genres')->group(function() {
  * NOTIFICATIONS
  */
 Route::prefix('notifications')->group(function() {
-    Route::get('/', NotificationController::class . "@index")->name('notifications.index');
-    Route::get('/{id}', NotificationController::class . "@show")->name('notifications.show');
-    Route::post('/', NotificationController::class . "@store")->name('notifications.store');
-    Route::put('/{id}', NotificationController::class . "@update")->name('notifications.update');
-    Route::delete('/{id}', NotificationController::class . "@delete")->name('notifications.delete');
+    Route::get('/', [NotificationController::class, "index"])->name('notifications.index');
+    Route::get('/{id}', [NotificationController::class, "show"])->name('notifications.show');
+    Route::post('/', [NotificationController::class, "store"])->name('notifications.store');
+    Route::put('/{id}', [NotificationController::class, "update"])->name('notifications.update');
+    Route::delete('/{id}', [NotificationController::class, "delete"])->name('notifications.delete');
 });
 
 
@@ -62,11 +63,11 @@ Route::prefix('notifications')->group(function() {
  * PRODUCTS
  */
 Route::prefix('products')->group(function() {
-    Route::get('/', ProductController::class . "@index")->name('products.index');
-    Route::get('/{id}', ProductController::class . "@show")->name('products.show');
-    Route::post('/', ProductController::class . "@store")->name('products.store');
-    Route::put('/{id}', ProductController::class . "@update")->name('products.update');
-    Route::delete('/{id}', ProductController::class . "@delete")->name('products.delete');
+    Route::get('/', [ProductController::class, "index"])->name('products.index');
+    Route::get('/{id}', [ProductController::class, "show"])->name('products.show');
+    Route::post('/', [ProductController::class, "store"])->name('products.store');
+    Route::put('/{id}', [ProductController::class, "update"])->name('products.update');
+    Route::delete('/{id}', [ProductController::class, "delete"])->name('products.delete');
 });
 
 
@@ -74,11 +75,11 @@ Route::prefix('products')->group(function() {
  * RATINGS
  */
 Route::prefix('ratings')->group(function() {
-    Route::get('/', RatingController::class . "@index")->name('ratings.index');
-    Route::get('/{id}', RatingController::class . "@show")->name('ratings.show');
-    Route::post('/', RatingController::class . "@store")->name('ratings.store');    
-    Route::put('/{id}', RatingController::class . "@update")->name('ratings.update');
-    Route::delete('/{id}', RatingController::class . "@delete")->name('ratings.delete');
+    Route::get('/', [RatingController::class, "index"])->name('ratings.index');
+    Route::get('/{id}', [RatingController::class, "show"])->name('ratings.show');
+    Route::post('/', [RatingController::class, "store"])->name('ratings.store');    
+    Route::put('/{id}', [RatingController::class, "update"])->name('ratings.update');
+    Route::delete('/{id}', [RatingController::class, "delete"])->name('ratings.delete');
 });
 
 
@@ -86,9 +87,9 @@ Route::prefix('ratings')->group(function() {
  * SALES
  */
 Route::prefix('sales')->group(function() {
-    Route::get('/', SaleController::class . "@index")->name('sales.index');
-    Route::get('/{id}', SaleController::class . "@show")->name('sales.show');
-    Route::post('/', SaleController::class . "@store")->name('sales.store');
-    Route::put('/{id}', SaleController::class . "@update")->name('sales.update');
-    Route::delete('/{id}', SaleController::class . "@delete")->name('sales.delete');
+    Route::get('/', [SaleController::class, "index"])->name('sales.index');
+    Route::get('/{id}', [SaleController::class, "show"])->name('sales.show');
+    Route::post('/', [SaleController::class, "store"])->name('sales.store');
+    Route::put('/{id}', [SaleController::class, "update"])->name('sales.update');
+    Route::delete('/{id}', [SaleController::class, "delete"])->name('sales.delete');
 });
