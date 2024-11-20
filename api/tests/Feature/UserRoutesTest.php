fbd583929179fedeb0c07932a13243ca2a7bc0bc
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2024-11-20 01:29:42 -0300

    Created basic User Test

--- api/tests/Feature/UserRoutesTest.php
+++ api/tests/Feature/UserRoutesTest.php
@@ -10,37 +10,45 @@ class UserRoutesTest extends TestCase
 {
 	private $baseUri = '/' . AbstractEnum::API_ROUTE_PREFIX . '/' . UserEnum::ROUTE_PREFIX;
 
-	// public function test_if_index_route_returns_successful() : void
-	// {
-	// 	$response = $this->get($this->baseUri);
+	public function test_if_index_route_returns_successful() : void
+	{
+		$response = $this->get($this->baseUri);
 
-	// 	$response->assertStatus(200);
-	// }
+		$response->assertStatus(200);
+	}
 
 	public function test_if_store_route_creates_resource_successfully(): void
 	{
+		$faker = \Faker\Factory::create();
+
 		$data = [
-			"name" => "PHPUnit User",
-			"email" => "phpunit_user@example.com",
-			"password" => "strongpass123!",
-			"password_confirmation" => "strongpass123!",
-			"cpf" => "12345678901",
-			"birth_date" => "1990-05-15",
-			"address" => "Flower's street",
-			"address_number" => "123",
-			"address_neighborhood" => "Green Garden",
-			"address_complement" => "---------------",
-			"address_zipcode" => "12345678",
-			"role" => UserEnum::CLIENT,
-			"phones" => [
-      			"phone" => "11987654321"
-			],
+			'name'                 => $faker->name,
+			'email'                => $faker->unique()->safeEmail,
+			'password'             => 'strongpass1234@!',
+			'password_confirmation'=> 'strongpass1234@!',
+			'cpf'                  => $faker->numberBetween(11111111111, 99999999999),
+			'birth_date'           => $faker->date('Y-m-d', '-18 years'),  // Data de nascimento (maior de idade)
+			'address'              => $faker->streetAddress,
+			'address_number'       => $faker->numberBetween(1, 200),
+			'address_neighborhood' => $faker->word,
+			'address_complement'   => $faker->optional()->secondaryAddress,
+			'address_zipcode'      => $faker->numberBetween(11111111, 99999999),
+			'role'                 => $faker->randomElement([UserEnum::CLIENT, UserEnum::EMPLOYEE]),
+			'phones' => [
+				["phone" => $faker->numberBetween(11111111111, 99999999999)]
+			]
 		];
 
 		$response = $this->post($this->baseUri, $data);
 
+		$user = json_decode($response->content(), true);
 		$response->assertStatus(201);
+
+		$phones = $data["phones"];
 		
-		$this->assertDatabaseHas('users', $data);
+		$this->assertDatabaseHas('users', $user);
+		$this->assertDatabaseHas('phones', [
+			"phone" => $phones[0]['phone']
+		]);
 	}
 }
