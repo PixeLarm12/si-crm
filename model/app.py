2ee8c21d0e08380d59bbe4e6ff3f02eeaa914f83
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2024-11-24 18:12:43 -0300

    Added flask get route to test model

--- model/app.py
+++ model/app.py
@@ -1,10 +1,17 @@
+import sys
+import os
+
+# Adicionar o diretório onde app.py está localizado ao sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+
 from flask import Flask
+from utils.ai import recommend_to_user
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Flask!"
+    return recommend_to_user()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
