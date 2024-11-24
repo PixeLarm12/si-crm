d2bf2b86b6318deb087f98ffd5ca9c7c2e2c2bb7
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2024-11-24 13:15:37 -0300

    Changed from json to nested array

--- api/app/Services/ReportService.php
+++ api/app/Services/ReportService.php
@@ -23,46 +23,44 @@ class ReportService
 			'version' => '2',
 		]);
 
-		$config = <<<EOD
-			{
-				type: 'bar',
-				data: {
-					labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-					datasets: [
-					{
-						label: 'Dataset 1',
-						backgroundColor: 'rgba(255, 99, 132, 0.5)',
-						borderColor: 'rgb(255, 99, 132)',
-						borderWidth: 1,
-						data: [-31, -70, -30, -33, -9, 14, -41],
-					},
-					{
-						label: 'Dataset 2',
-						backgroundColor: 'rgba(54, 162, 235, 0.5)',
-						borderColor: 'rgb(54, 162, 235)',
-						borderWidth: 1,
-						data: [73, 41, 29, 61, -65, 59, 38],
-					},
+		$config = [
+			"type" => 'bar',
+			"data" => [
+				"labels" => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+				"datasets" => [
+					[
+						"label" => 'Dataset 1',
+						"backgroundColor" => 'rgba(255, 99, 132, 0.5)',
+						"borderColor" => 'rgb(255, 99, 132)',
+						"borderWidth" => 1,
+						"data" => [-31, -70, -30, -33, -9, 14, -41],
 					],
-				},
-				options: {
-					title: {
-					display: true,
-					text: 'Bar Chart',
-					},
-					plugins: {
-					datalabels: {
-						anchor: 'center',
-						align: 'center',
-						color: '#666',
-						font: {
-						weight: 'normal',
-						},
-					},
-					},
-				},
-			}
-		EOD;
+					[
+						"label" => 'Dataset 2',
+						"backgroundColor" => 'rgba(54, 162, 235, 0.5)',
+						"borderColor" => 'rgb(54, 162, 235)',
+						"borderWidth" => 1,
+						"data" => [73, 41, 29, 61, -65, 59, 38],
+					]
+				],
+			],
+			"options" => [
+				"title" => [
+					"display" => true,
+					"text" => 'Bar Chart',
+				],
+				"plugins" => [
+					"datalabels" => [
+						"anchor" => 'center',
+						"align" => 'center',
+						"color" => '#666',
+						"font" => [
+							"weight" => 'normal',
+						]
+					]
+				]
+			]
+		];
 
 		$quickChart->setConfig($config);
 		return 'data:image/png;base64,' . base64_encode($quickChart->toBinary());
